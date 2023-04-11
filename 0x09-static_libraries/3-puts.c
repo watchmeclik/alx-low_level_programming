63087ee360f73c2a19933a6d324e30c1d63f32d2
watchmeclik <watchmeclik@gmail.com>
2023-04-10 17:01:17 -0700

    update

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _puts - Func
+ * @s: var
+ */
+
+void _puts(char *s)
+{
+	puts(s);
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/*
+ * _putchar - Func
+ * @c: var
+ */
+
+int _putchar(char c)
+{
+	return (putchar(c));
+}
