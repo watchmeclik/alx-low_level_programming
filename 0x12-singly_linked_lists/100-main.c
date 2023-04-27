81ae9969353abe6a47c44389759cc93f25b994b1
watchmeclik <watchmeclik@gmail.com>
2023-04-27 15:20:38 -0700

    update

---
+++ 0x12-singly_linked_lists/100-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("(A tortoise, sensing a hare's nature, challenges it to a race)\n");
+	return (0);
+}
